b057b0253cb34a9fb4efc5dc48d387a33519925e
Johnmal96 <johnmal96@yahoo.com>
2017-10-04 10:45:47 +0000

    lab3a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
  Rails.application.routes.draw do
   resources :microposts
   resources :users
-  root 'users#index'
- end
+  get 'pages/help'
+  get 'pages/home'
+  get 'pages/about'
+  root 'pages#home'
+ end 
